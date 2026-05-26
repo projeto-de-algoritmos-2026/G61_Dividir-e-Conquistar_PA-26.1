30cd976127009084f792bfa4cfb1e2832a0dc2d0
caio-venancio <caio.venancio784@gmail.com>
2026-05-26 19:05:09 -0300

    feat: adicionar setup

---
+++ template/newSystem.js
@@ -0,0 +1,7 @@
+// newSystem.js
+// import { world } from '../game.js';
+
+// world.registerSystem(function initSystem(world) {
+// cria entidades com world.createEntity()
+// adiciona componentes adequados a entidade com world.addComponent()
+// })
